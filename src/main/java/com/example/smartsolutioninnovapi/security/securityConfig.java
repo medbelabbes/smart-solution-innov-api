47d02cad53dbecb463d7ca01f2f0ff5b933a78b7
Mohamed Abdelillah Belabbes <m.belabbes@esi-sba.dz>
2021-12-26 23:21:14 +0100

    add authenticationManagerBean method

--- src/main/java/com/example/smartsolutioninnovapi/security/securityConfig.java
+++ src/main/java/com/example/smartsolutioninnovapi/security/securityConfig.java
@@ -1,7 +1,10 @@
 package com.example.smartsolutioninnovapi.security;
 
+import com.example.smartsolutioninnovapi.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +32,11 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(null);
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
-}
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }}
