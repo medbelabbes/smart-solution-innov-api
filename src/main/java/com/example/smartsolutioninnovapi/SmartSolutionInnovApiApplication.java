a3426b649bc5529155c899ee4ed65b375e5059a3
Mohamed Abdelillah Belabbes <m.belabbes@esi-sba.dz>
2021-12-26 23:04:10 +0100

    add security config and implement userdetailsService

--- src/main/java/com/example/smartsolutioninnovapi/SmartSolutionInnovApiApplication.java
+++ src/main/java/com/example/smartsolutioninnovapi/SmartSolutionInnovApiApplication.java
@@ -8,38 +8,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
-public class SmartSolutionInnovApiApplication implements CommandLineRunner {
-
-    @Autowired
-    private  UserService userService;
-
-    @Autowired
-    private  RoleService roleService;
+public class SmartSolutionInnovApiApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartSolutionInnovApiApplication.class, args);
     }
 
-
-    @Override
-    public void run(String... args) throws Exception {
-        roleService.saveRole(new Role(null, "ROLE_USER"));
-        roleService.saveRole(new Role(null, "ROLE_MANAGER"));
-        roleService.saveRole(new Role(null, "ROLE_ADMIN"));
-        roleService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-        userService.saveUser(new User(null, "Mohamed Abdelillah", "medbelabbes", "123456", new ArrayList<>()));
-        userService.saveUser(new User(null, "Djamel Eddine", "djamel", "123456", new ArrayList<>()));
-        userService.saveUser(new User(null, "Abdelhamid Kamel", "hamid", "123456", new ArrayList<>()));
-        userService.saveUser(new User(null, "Salim", "salim", "123456", new ArrayList<>()));
-
-        roleService.addRoleToUser("medbelabbes", "ROLE_SUPER_ADMIN");
-        roleService.addRoleToUser("djamel", "ROLE_ADMIN");
-        roleService.addRoleToUser("hamid", "ROLE_USER");
-        roleService.addRoleToUser("salim", "ROLE_USER");
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return  new BCryptPasswordEncoder();
     }
+
+    @Bean
+    CommandLineRunner run(UserService userService, RoleService roleService) {
+        return args -> {
+            roleService.saveRole(new Role(null, "ROLE_USER"));
+            roleService.saveRole(new Role(null, "ROLE_MANAGER"));
+            roleService.saveRole(new Role(null, "ROLE_ADMIN"));
+            roleService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "Mohamed Abdelillah", "medbelabbes", "123456", new ArrayList<>()));
+            userService.saveUser(new User(null, "Djamel Eddine", "djamel", "123456", new ArrayList<>()));
+            userService.saveUser(new User(null, "Abdelhamid Kamel", "hamid", "123456", new ArrayList<>()));
+            userService.saveUser(new User(null, "Salim", "salim", "123456", new ArrayList<>()));
+
+            roleService.addRoleToUser("medbelabbes", "ROLE_SUPER_ADMIN");
+            roleService.addRoleToUser("djamel", "ROLE_ADMIN");
+            roleService.addRoleToUser("hamid", "ROLE_USER");
+            roleService.addRoleToUser("salim", "ROLE_USER");
+        };
+    }
+
+
 }
--- src/main/java/com/example/smartsolutioninnovapi/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/smartsolutioninnovapi/services/impl/UserServiceImpl.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +26,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -40,5 +46,25 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
     }
 }
